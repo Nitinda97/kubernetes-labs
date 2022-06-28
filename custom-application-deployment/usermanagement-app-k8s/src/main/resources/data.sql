136b9f9c9d725c76dd410742da91fc6db59ea4df
Nitin Khare <nitkhare@publicisgroupe.net>
2022-06-28 15:10:55 +0530

    updated

--- custom-application-deployment/usermanagement-app-k8s/src/main/resources/data.sql
+++ custom-application-deployment/usermanagement-app-k8s/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO webappdb.user (userid, email_address, first_name, last_name, password, ssn, user_name) 
-VALUES ("101", "admin101@stacksimplify.com", "Kalyan", "Reddy", "$2a$10$w.2Z0pQl9K5GOMVT.y2Jz.UW4Au7819nbzNh8nZIYhbnjCi6MG8Qu", "ssn101", "admin101");	
-INSERT INTO webappdb.role (roleid, role) VALUES ("201", "ADMIN");
-INSERT INTO webappdb.user_role (userid, roleid) VALUES ("101", "201");
+INSERT INTO userdb.user (userid, email_address, first_name, last_name, password, ssn, user_name) 
+VALUES ("101", "nitin@gmail.com", "Nitin", "Khare", "$2a$10$w.2Z0pQl9K5GOMVT.y2Jz.UW4Au7819nbzNh8nZIYhbnjCi6MG8Qu", "ssn101", "nitin");	
+INSERT INTO userdb.role (roleid, role) VALUES ("201", "ADMIN");
+INSERT INTO userdb.user_role (userid, roleid) VALUES ("101", "201");
